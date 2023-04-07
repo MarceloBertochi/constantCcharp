51f563daedfc9ba0006fbb8ed4a5f448d19e5056
MarceloBertochi <paidasamia@gmail.com>
2023-04-07 19:40:06 -0300

    Variável const

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace constante
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+           const int numero = 18;
+            Console.WriteLine(numero);            
+        }
+    }
+}
